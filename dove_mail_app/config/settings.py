0c6f441dfd1b78f5b1be97ebe5ff8f66b0368bc2
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-18 16:42:18 +0100

    installed dependencies and created a base path for core_app

--- dove_mail_app/config/settings.py
+++ dove_mail_app/config/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user_app',
+    'core_app',
+    'crispy_forms',
+    'markdownify',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

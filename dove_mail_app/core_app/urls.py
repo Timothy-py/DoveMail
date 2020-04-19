2b2a5f0fb70585f42d4c3eb113af7a257214d971
Timothy-py <adeyeyetimothy33@gmail.com>
2020-04-19 21:41:53 +0100

    created a view responsible for creating mailing list, created the template and connect with the url

--- dove_mail_app/core_app/urls.py
+++ dove_mail_app/core_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MailingListView
+from .views import MailingListView, CreateMailingListView
 
 app_name = "core_app"
 
 urlpatterns = [
-    path('', MailingListView.as_view(), name='mailing_list')
+    path('', MailingListView.as_view(), name='mailing_list'),
+    path('new', CreateMailingListView.as_view(), name='create_mailinglist'),
 ]
--- dove_mail_app/core_app/views.py
+++ dove_mail_app/core_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from .models import MailingList
+from .forms import MailingListForm
 
 # Create your views here.
 
@@ -14,3 +15,11 @@ class MailingListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return MailingList.objects.filter(owner=self.request.user)
+
+
+class CreateMailingListView(LoginRequiredMixin, CreateView):
+    form_class = MailingListForm
+    template_name = 'mailinglist_form.html'
+
+    def get_initial(self):
+        return {'owner': self.request.id}
